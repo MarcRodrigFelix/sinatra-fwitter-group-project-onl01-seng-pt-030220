3e73083eb425045dd85e756a9d85284ecfc364d3
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-28 20:28:31 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,48 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if !logged_in?
+      redirect "/login"
+    end
+    @tweets = Tweet.all
+    @user = current_user
+  binding.pry
+    erb :'/tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    if !logged_in?
+      redirect '/login'
+    end
+      erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+    if logged_in?
+      @tweet = Tweet.new(:content => params[:content])
+      @tweet.user = current_user
+        if @tweet.save
+          redirect '/tweets'
+        end
+    else
+      redirect "/tweets/new"
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'/tweets/edit_tweet'
+  end
+
+  patch '/tweets/:id' do
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,61 @@
 class UsersController < ApplicationController
 
+  get '/' do
+    @users = User.all
+    erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    end
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      logout!
+      redirect "/login"
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/logout' do
+    if logged_in?
+      redirect '/logout'
+    else
+      redirect '/login'
+    end
+  end
 
 end
